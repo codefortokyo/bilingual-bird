57a5e0368f368af21dab1dc0a0421e23e902a632
osoken <osoken.devel@outlook.jp>
2016-04-08 22:54:13 +0900

    add loopback

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-var translate = require('../translate');
-var twitter = require('twitter');
+var loopback = require('../loopback');
+var tweet = require('../tweet');
 
 var config = require('../config');
 
 
 router.get('/translate', function(req, res, next) {
-
+  if (req.query.text === void 0)
+  {
+    return res.end('text is required');
+  }
+  if (req.query.text.length === 0)
+  {
+    return res.json({ja: '', en: ''});
+  }
+  loopback(req.query.text, function(e, d)
+  {
+    if (e != null)
+    {
+      return res.end(e);
+    }
+    return res.json(d);
+  });
 });
 
 router.post('/tweet', function(req, res, next) {
+
 });
 
 module.exports = router;
