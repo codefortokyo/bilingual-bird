b5618a878ad83e5a23eb56fccbc9f4ab7869565d
osoken <osoken.devel@outlook.jp>
2016-04-13 00:58:47 +0900

    add header and footer

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,7 +9,13 @@
     {
       root = d3.select(_);
     }
+    var header = root.append('header');
+    header.append('div').text('duplicitous bird');
     var article = root.append('article');
+    var footer = root.append('footer');
+    footer.append('span').style('margin-right', '4px').text('made with');
+    footer.append('i').attr('class', 'fa fa-heart').attr('aria-hidden', true);
+    footer.append('span').style('margin-left', '4px').text('by Code for Tokyo.');
     var input = article.append('textarea');
     var cog = article.append('i')
       .attr('class', 'fa fa-cog fa-spin')
@@ -17,26 +23,39 @@
 
     var en = article.append('textarea');
     var ja = article.append('textarea');
+    var tweet = article.append('a');
+    tweet.attr('href', '#')
+      .on('click', function() {
+        d3.event.preventDefault();
+        d3.json('/api/tweet')
+          .header('Content-Type', 'application/json')
+          .post(JSON.stringify({
+            ja: ja.property('value'),
+            en: en.property('value')
+          }), function(e, d) {
+            if (e != null) {
+              return;
+            }
+          });
+      });
+    tweet.append('i')
+      .attr('class', 'fa fa-twitter')
+      .attr('aria-hidden', true);
     var timer = null;
-    var tr = function()
-    {
+    var tr = function() {
       d3.json('/api/translate?text='+input.property('value'),
-              function(e, d)
-              {
-                if (e != null)
-                {
+              function(e, d) {
+                if (e != null) {
                   return;
                 }
-                en.text(d.en);
-                ja.text(d.ja);
+                en.property('value', d.en);
+                ja.property('value', d.ja);
                 timer = null;
                 cog.style('visibility', 'hidden');
               });
     };
-    input.on('keyup', function()
-    {
-      if (timer != null)
-      {
+    input.on('keyup', function() {
+      if (timer != null) {
         clearTimeout(timer);
       }
       cog.style('visibility', 'visible');
