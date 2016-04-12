bc21a129434e65106b3713a3b8dbe2b1ac425a09
osoken <osoken.devel@outlook.jp>
2016-04-13 01:32:06 +0900

    add new translate api and renamed old translate endpoint to loopback

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var translate = require('../translate');
 var loopback = require('../loopback');
 var bird = require('../duplicity');
 
 router.get('/translate', function(req, res) {
+  if (req.query.text === void 0)
+  {
+    return res.end('text is required');
+  }
+  if (req.query.text.length === 0)
+  {
+    return res.json({ja: '', en: ''});
+  }
+  translate({text: req.query.text,
+             from: 'ja',
+             to: 'en'}, function(e, d)
+  {
+    if (e != null)
+    {
+      return res.end(e);
+    }
+    return res.json({en: d});
+  });
+});
+
+router.get('/loopback', function(req, res) {
   if (req.query.text === void 0)
   {
     return res.end('text is required');
